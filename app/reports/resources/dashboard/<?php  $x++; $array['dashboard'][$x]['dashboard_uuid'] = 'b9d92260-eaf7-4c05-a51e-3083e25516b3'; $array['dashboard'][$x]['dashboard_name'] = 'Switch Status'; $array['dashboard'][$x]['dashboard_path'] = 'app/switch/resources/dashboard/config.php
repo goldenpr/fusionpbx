afc30cd8cbd19345ef8aff593da53380c17c4414
Golden <42316874+goldenpr@users.noreply.github.com>
2023-01-29 16:25:53 +0200

    Update config.php

--- app/reports/resources/dashboard/<?php  $x++; $array['dashboard'][$x]['dashboard_uuid'] = 'b9d92260-eaf7-4c05-a51e-3083e25516b3'; $array['dashboard'][$x]['dashboard_name'] = 'Switch Status'; $array['dashboard'][$x]['dashboard_path'] = 'app/switch/resources/dashboard/config.php
+++ app/reports/resources/dashboard/<?php  $x++; $array['dashboard'][$x]['dashboard_uuid'] = 'b9d92260-eaf7-4c05-a51e-3083e25516b3'; $array['dashboard'][$x]['dashboard_name'] = 'Switch Status'; $array['dashboard'][$x]['dashboard_path'] = 'app/switch/resources/dashboard/config.php
@@ -1,15 +1,15 @@
 <?php
 
 $x++;
-$array['dashboard'][$x]['dashboard_uuid'] = 'b9d92260-eaf7-4c05-a51e-3083e25516b3';
-$array['dashboard'][$x]['dashboard_name'] = 'Switch Status';
-$array['dashboard'][$x]['dashboard_path'] = 'app/switch/resources/dashboard/switch_status.php';
-$array['dashboard'][$x]['dashboard_order'] = '105';
-$array['dashboard'][$x]['dashboard_enabled'] = 'false';
-$array['dashboard'][$x]['dashboard_description'] = 'Switch details such as version, uptime, channels and registrations.';
+$array['dashboard'][$x]['dashboard_uuid'] = 'b066ae7f-ff6e-4762-ada8-901a8025d15c';
+$array['dashboard'][$x]['dashboard_name'] = 'Bill Summery';
+$array['dashboard'][$x]['dashboard_path'] = 'app/reports/resources/dashboard/bill.php';
+$array['dashboard'][$x]['dashboard_order'] = '10';
+$array['dashboard'][$x]['dashboard_enabled'] = 'true';
+$array['dashboard'][$x]['dashboard_description'] = 'bill summery of the client.';
 $y = 0;
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = '5469c403-2cc5-4340-99f8-6f0755407de7';
-$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'b9d92260-eaf7-4c05-a51e-3083e25516b3';
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'b066ae7f-ff6e-4762-ada8-901a8025d15c';
 $array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'superadmin';
 $x++;
 
