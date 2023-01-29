34ec4f9964b0d63eb2547052ac471575837b8472
Golden <42316874+goldenpr@users.noreply.github.com>
2023-01-29 15:44:14 +0200

    TEST2

---
+++ app/reports/app_menu.php
@@ -0,0 +1,28 @@
+<?php
+	$y=0;
+	$apps[$x]['menu'][$y]['title']['en-us'] = "Bills Report";
+	$apps[$x]['menu'][$y]['title']['en-gb'] = "Bills Report";
+	$apps[$x]['menu'][$y]['title']['ar-eg'] = "";
+	$apps[$x]['menu'][$y]['title']['de-at'] = "Bills Report";
+	$apps[$x]['menu'][$y]['title']['de-de'] = "Bills Report";
+	$apps[$x]['menu'][$y]['title']['es-cl'] = "Bills Report";
+	$apps[$x]['menu'][$y]['title']['es-mx'] = "";
+	$apps[$x]['menu'][$y]['title']['fr-ca'] = "";
+	$apps[$x]['menu'][$y]['title']['fr-fr'] = "Bills Report";
+	$apps[$x]['menu'][$y]['title']['he-il'] = "שטרות";
+	$apps[$x]['menu'][$y]['title']['it-it'] = "Bills Report";
+	$apps[$x]['menu'][$y]['title']['nl-nl'] = "Bills Report";
+	$apps[$x]['menu'][$y]['title']['pl-pl'] = "Bills Report";
+	$apps[$x]['menu'][$y]['title']['pt-br'] = "Bills Report";
+	$apps[$x]['menu'][$y]['title']['pt-pt'] = "Bills Report";
+	$apps[$x]['menu'][$y]['title']['ro-ro'] = "";
+	$apps[$x]['menu'][$y]['title']['ru-ru'] = "Bills Report";
+	$apps[$x]['menu'][$y]['title']['sv-se'] = "Bills Report";
+	$apps[$x]['menu'][$y]['title']['uk-ua'] = "Bills Report";
+	$apps[$x]['menu'][$y]['uuid'] = "156fdebd-a2dd-46b3-8b0f-018ef6a9d68b";
+	$apps[$x]['menu'][$y]['parent_uuid'] = "e1170753-e1cb-44d6-90cc-6cf4d242092a";
+	$apps[$x]['menu'][$y]['category'] = "internal";
+	$apps[$x]['menu'][$y]['path'] = "/app/reports/bills_report.php?direction=outbound";
+	$apps[$x]['menu'][$y]['groups'][] = "admin";
+	$apps[$x]['menu'][$y]['groups'][] = "superadmin";
+?>
