2527df305e7642d634d86f4c6eabffe0be7ec7b6
Golden <42316874+goldenpr@users.noreply.github.com>
2023-01-29 17:20:55 +0200

    Update config.php

--- app/reports/resources/dashboard/config.php
+++ app/reports/resources/dashboard/config.php
@@ -4,7 +4,7 @@ $x++;
 $array['dashboard'][$x]['dashboard_uuid'] = 'b066ae7f-ff6e-4762-ada8-901a8025d15c';
 $array['dashboard'][$x]['dashboard_name'] = 'Bill Summery';
 $array['dashboard'][$x]['dashboard_path'] = 'app/reports/resources/dashboard/bill.php';
-$array['dashboard'][$x]['dashboard_order'] = '10';
+$array['dashboard'][$x]['dashboard_order'] = '9';
 $array['dashboard'][$x]['dashboard_enabled'] = 'true';
 $array['dashboard'][$x]['dashboard_description'] = 'bill summery of the client.';
 $y = 0;
